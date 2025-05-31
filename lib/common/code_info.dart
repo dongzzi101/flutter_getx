cdf76e35dd4d0cd0ac681c992c470d1894d0927a
dongwook <dongzzi101@gmail.com>
2025-05-31 20:18:27 +0900

    post dao

---
+++ lib/common/code_info.dart
@@ -0,0 +1,1 @@
+enum CodeInfo { ok, err }
---
+++ lib/dao/post_dao.dart
@@ -0,0 +1,47 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter_getx/api/api_info.dart';
+import 'package:flutter_getx/common/code_info.dart';
+import 'package:flutter_getx/dto/post_bean.dart';
+import 'package:get/get.dart';
+import 'package:get/get_connect/http/src/request/request.dart';
+
+class PostDao extends GetConnect {
+  @override
+  void onInit() {
+    super.onInit();
+
+    httpClient.baseUrl = ApiInfo.rootPostUrl;
+    httpClient.defaultContentType = 'application/x-www-form-urlencoded';
+    httpClient.addRequestModifier((Request request) {
+      request.headers['Accept'] = 'application/json';
+      return request;
+    });
+  }
+
+  Future<Map<String, dynamic>> getPosts() async {
+    Map<String, dynamic> resultMap = <String, dynamic>{};
+    CodeInfo code = CodeInfo.ok;
+
+    List<PostBean> list = [];
+
+    try {
+      final Response res = await get('/posts');
+
+      if (res.statusCode == HttpStatus.ok) {
+        list =
+            res.body.map<PostBean>((json) => PostBean.fromJson(json)).toList();
+      } else {
+        code = CodeInfo.err;
+      }
+    } catch (e) {
+      code = CodeInfo.err;
+    }
+
+    resultMap['code'] = code;
+    resultMap['list'] = list;
+
+    return resultMap;
+  }
+}
