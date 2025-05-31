aa037fe625f8121733c49369db8cb881cd97b9ff
dongwook <dongzzi101@gmail.com>
2025-05-31 20:02:05 +0900

    로딩 위젯

--- lib/view/home/home_view.dart
+++ lib/view/home/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_getx/component/loading_widget.dart';
 import 'package:flutter_getx/controller/home_controller.dart';
 import 'package:get/get.dart';
 
@@ -10,55 +11,63 @@ class HomeView extends GetView<HomeController> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
+        child: Stack(
           children: [
-            Card(
-              margin: const EdgeInsets.all(16.0),
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Row(
-                  children: [
-                    Image.asset('assets/img/flutter.png', width: 24.0),
-                    SizedBox(width: 16.0),
-                    Expanded(child: Text('home.card.1'.tr))
-                  ],
-                ),
-              ),
-            ),
-            Card(
-              margin: const EdgeInsets.all(16.0),
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: SizedBox(
-                  width: double.infinity,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const FlutterLogo(),
-                      const SizedBox(height: 16.0),
-                      Text('home.card.2'.tr)
-                    ],
+            Column(
+              children: [
+                Card(
+                  margin: const EdgeInsets.all(16.0),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Row(
+                      children: [
+                        Image.asset('assets/img/flutter.png', width: 24.0),
+                        SizedBox(width: 16.0),
+                        Expanded(child: Text('home.card.1'.tr))
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ),
-            Card(
-              margin: const EdgeInsets.all(16.0),
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: SizedBox(
-                  width: double.infinity,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Icon(Icons.flutter_dash, color: Colors.lightBlue),
-                      const SizedBox(height: 16.0,),
-                      Text('home.card.3'.tr)
-                    ],
+                Card(
+                  margin: const EdgeInsets.all(16.0),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: SizedBox(
+                      width: double.infinity,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const FlutterLogo(),
+                          const SizedBox(height: 16.0),
+                          Text('home.card.2'.tr)
+                        ],
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            )
+                Card(
+                  margin: const EdgeInsets.all(16.0),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: SizedBox(
+                      width: double.infinity,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Icon(Icons.flutter_dash,
+                              color: Colors.lightBlue),
+                          const SizedBox(
+                            height: 16.0,
+                          ),
+                          Text('home.card.3'.tr)
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Obx(() => LoadingWidget(controller.isLoading.value)),
           ],
         ),
       ),
